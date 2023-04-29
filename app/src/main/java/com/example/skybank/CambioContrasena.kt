3bd87c23d01d44feed438e3cd1ad4e72e8c79add
carlo <20220367@ricaldone.edu.sv>
2023-04-28 22:59:30 -0600

    Pantalla de cambiar contraseña

---
+++ app/src/main/java/com/example/skybank/CambioContrasena.kt
@@ -0,0 +1,11 @@
+package com.example.skybank
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CambioContrasena : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cambio_contrasena)
+    }
+}
