8284e1ac9074cef614b2bb204dba301f3dcfa001
dhakhax <af639012@gmail.com>
2023-05-02 22:08:50 -0600

    pantalla de agendar citas (sin programar)

---
+++ app/src/main/java/com/example/skybank/AgendarCitas.kt
@@ -0,0 +1,11 @@
+package com.example.skybank
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AgendarCitas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agendar_citas)
+    }
+}
