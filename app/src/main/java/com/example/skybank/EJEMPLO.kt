1ed08278cf538128ba90b7948ad07f7fe88819c9
dhakhax <af639012@gmail.com>
2023-05-07 15:11:12 -0600

    se agrego x pantalla

---
+++ app/src/main/java/com/example/skybank/EJEMPLO.kt
@@ -0,0 +1,11 @@
+package com.example.skybank
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EJEMPLO : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ejemplo)
+    }
+}
