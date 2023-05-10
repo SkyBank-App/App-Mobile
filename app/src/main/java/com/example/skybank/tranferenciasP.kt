327e43a68e044b8972f6e61ba951556cc8867778
dhakhax <af639012@gmail.com>
2023-05-10 12:53:34 -0600

    se termino el diseño de transferencias

---
+++ app/src/main/java/com/example/skybank/tranferenciasP.kt
@@ -0,0 +1,11 @@
+package com.example.skybank
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class tranferenciasP : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tranferencias_p)
+    }
+}
