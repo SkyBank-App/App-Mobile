504463429d1005bb580f542f1f261d70d59d090c
dhakhax <af639012@gmail.com>
2023-05-10 18:25:37 -0600

    se termino el diseño de verificar codigo

---
+++ app/src/main/java/com/example/skybank/verificar_codigo.kt
@@ -0,0 +1,11 @@
+package com.example.skybank
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class verificar_codigo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verificar_codigo)
+    }
+}
